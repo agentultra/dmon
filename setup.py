50ec56b6ef4a423182b3e9afa935cf0e7e68a150
James King <james@agentultra.com>
2013-05-07 15:32:45 -0400

    Downgraded to Python 2.7 to use eventlet
    Ultimately I'd like to experiment with integrating this project into
    OpenStack which is still based on Python 2.x and uses eventlet for
    pretty much everything.

--- setup.py
+++ setup.py
@@ -1,1 +1,27 @@
-# setup
+from setuptools import setup, find_packages
+
+
+__author__ = "James King"
+__email__ = "james@agentultra.com"
+__version__ = "0.1.1"
+__license__ = "MIT"
+
+
+setup(
+    name="dmon",
+    version=__version__,
+    packages=find_packages(),
+
+    install_requires = [
+        'eventlet',
+    ],
+
+    author=__author__,
+    author_email=__email__,
+    license=__license__,
+    description="A stream processing service for monitoring distributed clusters.",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
