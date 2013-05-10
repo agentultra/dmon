3b3847a986f5954ada7b6fd3b5e987c690b3e58b
Scott Moynes <scott.moynes@gmail.com>
2013-05-10 19:01:37 +0000

    Add interface for Index.

---
+++ dmon/index.py
@@ -0,0 +1,33 @@
+class Index(object):
+    def __init__(self):
+        pass
+
+    def clear(self):
+        """Resets the index"""
+        raise NotImplementedError()
+
+    def delete(self, event):
+        """Deletes any event in the index with a matching host and service.
+
+        Returns the deleted event, if found; None, otherwise.
+        """
+        raise NotImplementedError()
+
+    def expire(self):
+        """Removes all expired events from the index.
+
+        Returns a list of events removed from the index.
+        """
+        raise NotImplementedError()
+
+    def search(self, query):
+        """Returns a list of events matching a query AST."""
+        raise NotImplementedError()
+
+    def update(self, event):
+        """Adds an event to the index"""
+        raise NotImplementedError()
+
+    def get(self, host, service):
+        """Finds an event in the index."""
+        raise NotImplementedError()
---
+++ tests/test_index.py
@@ -0,0 +1,52 @@
+from unittest import TestCase
+from nose.tools import raises
+
+from dmon.index import Index
+from dmon.event import Event
+
+
+class IndexTestCase(TestCase):
+    def setUp(self):
+        self.index = Index()
+
+    @raises(NotImplementedError)
+    def test_clear(self):
+        self.index.clear()
+
+    @raises(NotImplementedError)
+    def test_delete(self):
+        event = self.create_event()
+        self.index.delete(event)
+
+    @raises(NotImplementedError)
+    def test_expire(self):
+        event = self.create_event()
+        self.index.expire()
+
+    @raises(NotImplementedError)
+    def test_search(self):
+        query_ast = []
+        self.index.search(query_ast)
+
+    @raises(NotImplementedError)
+    def test_update(self):
+        event = self.create_event()
+        self.index.update(event)
+
+    @raises(NotImplementedError)
+    def test_get(self):
+        self.index.get('host.name', 'service name')
+
+    def create_event(self, **kwargs):
+        default_event_attrs = {
+            'host': 'host.name',
+            'service': 'service name',
+            'state': 'up',
+            'description': 'text description',
+            'time': 1368210977,
+            'ttl': 600,
+            'metric': 'tps'
+        }
+        event_attrs = {attr: kwargs.get(attr, default_event_attrs[attr])
+                       for attr in default_event_attrs.iterkeys()}
+        event = Event(**event_attrs)
