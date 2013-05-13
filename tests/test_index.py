5d448f888c4afc43ddede35a4109c98f71aae0ad
Scott Moynes <scott.moynes@gmail.com>
2013-05-13 21:10:28 +0000

    Move IndexTestCase.create_event to base test class.

--- tests/test_index.py
+++ tests/test_index.py
@@ -2,10 +2,11 @@ from unittest import TestCase
 from nose.tools import raises
 
 from dmon.index import Index
-from dmon.event import Event
+
+from .base import BaseTestCase
 
 
-class IndexTestCase(TestCase):
+class IndexTestCase(BaseTestCase):
     def setUp(self):
         self.index_dict = {}
         self.index = Index(self.index_dict)
@@ -58,21 +59,6 @@ class IndexTestCase(TestCase):
         found_event = self.index.get(*event_key)
         self.assertEquals(found_event, None)
 
-    def create_event(self, **kwargs):
-        default_event_attrs = {
-            'host': 'host.name',
-            'service': 'service name',
-            'state': 'up',
-            'description': 'text description',
-            'time': 1368210977,
-            'ttl': 600,
-            'metric': 'tps'
-        }
-        event_attrs = {attr: kwargs.get(attr, default_event_attrs[attr])
-                       for attr in default_event_attrs.iterkeys()}
-        event = Event(**event_attrs)
-        return event
-
     @raises(NotImplementedError)
     def test_expire(self):
         event = self.create_event()
@@ -82,4 +68,3 @@ class IndexTestCase(TestCase):
     def test_search(self):
         query_ast = []
         self.index.search(query_ast)
-
